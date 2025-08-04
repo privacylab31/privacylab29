f35881efe272835142963b07fe741314b5369e62
privacylab31 <privacylab31@gmail.com>
2025-08-04 15:20:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,25 @@ function openPrivacyNotice() {
             </style>
         </head>
         <body>
-          
+       <!-- OneTrust Privacy Notice start -->
+  <!-- Language Drop-down element that will control in which language the Privacy Notice is displayed -->
+  <div class="ot-privacy-notice-language-dropdown-container"></div>
+  <!-- Container in which the Privacy Notice will be rendered -->
+  <div id="otnotice-21ef95f6-f18b-48cc-a112-5078584a509c" class="otnotice"></div>
+
+  <script src="https://privacyportal-in-cdn.onetrust.com/privacy-notice-scripts/otnotice-1.0.min.js" type="text/javascript" charset="UTF-8" id="otprivacy-notice-script">
+      settings="eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1pbi5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtaW4ub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1pbi5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0="
+    </script>
+
+  <script type="text/javascript" charset="UTF-8">
+      // To ensure external settings are loaded, use the Initialized promise:
+      OneTrust.NoticeApi.Initialized.then(function() {
+        OneTrust.NoticeApi.LoadNotices(["https://privacyportal-in-cdn.onetrust.com/storage-container/53ec83ca-0693-46f3-a55b-110c3f8f5a64/privacy-notices/21ef95f6-f18b-48cc-a112-5078584a509c/published/privacynotice.json"]);
+      });
+    </script>
+
+  <!-- OneTrust Privacy Notice end -->
+   
 
         </body>
         </html>
@@ -35,3 +53,4 @@ function openPrivacyNotice() {
     // Close the document to finish writing and render the content
     privacyWindow.document.close();
 }
+
