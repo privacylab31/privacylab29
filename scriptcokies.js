11a5c199a1b77601992cad89712a559b57a53351
privacylab31 <privacylab31@gmail.com>
2026-01-05 14:32:56 +0530

    Update scriptcokies.js

--- scriptcokies.js
+++ scriptcokies.js
@@ -20,7 +20,24 @@ function openCookiePrivacyNotice() {
         <body>
      
 
-<div id="ot-sdk-cookie-policy"></div>
+<!-- OneTrust Privacy Notice start -->
+  <!-- Language Drop-down element that will control in which language the Privacy Notice is displayed -->
+  <div class="ot-privacy-notice-language-dropdown-container"></div>
+  <!-- Container in which the Privacy Notice will be rendered -->
+  <div id="otnotice-ac51edf1-f597-40c2-b6d1-b8fb035b1deb" class="otnotice"></div>
+
+  <script src="https://privacyportal-in-cdn.onetrust.com/privacy-notice-scripts/otnotice-1.0.min.js" type="text/javascript" charset="UTF-8" id="otprivacy-notice-script">
+      settings="eyJjYWxsYmFja1VybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1pbi5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9wcml2YWN5Tm90aWNlcy9zdGF0cy92aWV3cyIsImNvbnRlbnRBcGlVcmwiOiJodHRwczovL3ByaXZhY3lwb3J0YWwtaW4ub25ldHJ1c3QuY29tL3JlcXVlc3QvdjEvZW50ZXJwcmlzZXBvbGljeS9kaWdpdGFscG9saWN5L2NvbnRlbnQiLCJtZXRhZGF0YUFwaVVybCI6Imh0dHBzOi8vcHJpdmFjeXBvcnRhbC1pbi5vbmV0cnVzdC5jb20vcmVxdWVzdC92MS9lbnRlcnByaXNlcG9saWN5L2RpZ2l0YWxwb2xpY3kvbWV0YS1kYXRhIn0="
+    </script>
+
+  <script type="text/javascript" charset="UTF-8">
+      // To ensure external settings are loaded, use the Initialized promise:
+      OneTrust.NoticeApi.Initialized.then(function() {
+        OneTrust.NoticeApi.LoadNotices(["https://privacyportal-in-cdn.onetrust.com/storage-container/53ec83ca-0693-46f3-a55b-110c3f8f5a64/privacy-notices/ac51edf1-f597-40c2-b6d1-b8fb035b1deb/published/privacynotice.json"]);
+      });
+    </script>
+
+  <!-- OneTrust Privacy Notice end -->
 
   
   
